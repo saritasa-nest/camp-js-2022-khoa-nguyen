87ac344f396acaf34f8600579cd766a1377bfa72
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-08 17:40:48 +0700

    Remove draft in fetch anime function #JC19-353

--- apps/vanilla/src/scripts/fetchAnimeList.ts
+++ apps/vanilla/src/scripts/fetchAnimeList.ts
@@ -9,21 +9,6 @@ import { Pagination } from '@js-camp/core/models/pagination';
 import { appAxios } from '../configs';
 import { PaginationOptions } from '../interface/paginationInterface';
 
-// /**
-//  * Fetch anime data with corresponding limit, offset and ordering.
-//  * @param limit Anime quantity per pages.
-//  * @param offset Anime offset.
-//  * @param ordering Anime ordering.
-//  */
-// export async function fetchAnimeList(limit: number, offset: number, ordering: string): Promise<Pagination<Anime>> {
-//   try {
-//     const result = await appAxios.get<PaginationDto<AnimeDto>>(`anime/anime/`, { params: { limit, offset, ordering } });
-//     return PaginationMapper.fromDto<AnimeDto, Anime>(result.data, AnimeMapper.fromDto);
-//   } catch (errors: unknown) {
-//     throw new Error(`Failed to get anime list: ${errors as string}`);
-//   }
-// }
-
 /**
  * Fetch anime data with corresponding limit, offset and ordering.
  * @param options Options settings of pagination.
