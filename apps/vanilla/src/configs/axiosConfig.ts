ebd2b8b2a58e298c02e3e37dedfef3dcdf8b0a1b
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-08 15:03:27 +0700

    Remove element selectors #JC19-353

--- apps/vanilla/src/configs/axiosConfig.ts
+++ apps/vanilla/src/configs/axiosConfig.ts
@@ -1,32 +1,10 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 import { API_KEY, BASE_URL } from '../constants';
 
-/**
- * Interceptor that adds the schema API key header to the request.
- * @param config Axios config.
- */
-function schemaInterceptor(config: AxiosRequestConfig): AxiosRequestConfig {
-  const { headers } = config;
-
-  if (headers === null) {
-    throw new Error(
-      'Axios did not pass any header. Please check your request.',
-    );
-  }
-
-  return {
-    ...config,
-    headers: {
-      ...headers,
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      'api-key': API_KEY,
-    },
-  };
-}
-
 export const appAxios: AxiosInstance = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'api-key': API_KEY,
+  },
 });
-
-appAxios.interceptors.request.use(schemaInterceptor);
