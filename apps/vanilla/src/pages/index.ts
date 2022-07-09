2dbc78033722197b1e3e9f0c001c2230c83971c4
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-09 22:33:38 +0700

    Rename constants in renderPaginationItems function #JC19-353

--- apps/vanilla/src/pages/index.ts
+++ apps/vanilla/src/pages/index.ts
@@ -102,16 +102,16 @@ function renderRangeOfPagination(activePage: number, from: number, to: number): 
 function renderPaginationItems(): string {
   const { activePage, totalPages } = PAGINATION_OPTIONS;
   const FIRST_PAGE = 1;
-  const PAGE_AT_INDEX_5 = 5;
-  const PAGE_AT_INDEX_5_REVERSE = totalPages - 4;
+  const PAGE_5 = 5;
+  const PAGE_5_DESCENDING = totalPages - 4;
   const PAGE_STEP = 2;
 
   if (activePage <= PAGE_STEP) {
-    return renderRangeOfPagination(activePage, FIRST_PAGE, PAGE_AT_INDEX_5);
+    return renderRangeOfPagination(activePage, FIRST_PAGE, PAGE_5);
   } else if (activePage > PAGE_STEP && activePage < totalPages - PAGE_STEP) {
     return renderRangeOfPagination(activePage, activePage - PAGE_STEP, activePage + PAGE_STEP);
   } else if (activePage >= totalPages - PAGE_STEP) {
-    return renderRangeOfPagination(activePage, PAGE_AT_INDEX_5_REVERSE, totalPages);
+    return renderRangeOfPagination(activePage, PAGE_5_DESCENDING, totalPages);
   }
   return '';
 }
