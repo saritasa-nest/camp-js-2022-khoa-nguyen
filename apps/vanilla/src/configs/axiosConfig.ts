21715d8bb82448ac6f35fcf2a23911c42edb5b1c
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-13 12:58:00 +0700

    Remove unnessary type annotation in appAxios constant #JC19-353

--- apps/vanilla/src/configs/axiosConfig.ts
+++ apps/vanilla/src/configs/axiosConfig.ts
@@ -1,8 +1,8 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
 import { API_KEY, BASE_URL } from '../constants';
 
-export const appAxios: AxiosInstance = axios.create({
+export const appAxios = axios.create({
   baseURL: BASE_URL,
   headers: {
     'api-key': API_KEY,
