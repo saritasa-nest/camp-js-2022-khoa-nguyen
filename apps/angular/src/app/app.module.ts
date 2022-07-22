793c76c0ce3e0047599e4350ffcfd457a68d4b62
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-22 13:24:33 +0700

    Retry linting #JC19-358

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './../shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 /** App module. */
 @NgModule({
 
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
