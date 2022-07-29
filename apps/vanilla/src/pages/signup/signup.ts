9c52e9efd894f3d871d30a6fcbf1fb67c6544256
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-29 22:58:47 +0700

    Add early return #JC19-358

--- apps/vanilla/src/pages/signup/signup.ts
+++ apps/vanilla/src/pages/signup/signup.ts
@@ -7,44 +7,44 @@ import { AuthorizationService } from '../../services/authorization';
 /** Validate register info. */
 function validateRegisterInfo(): void {
   const form = document.querySelector<HTMLFormElement>('.form');
-  if (form !== null) {
-    const inputEmail = form.querySelector<HTMLInputElement>('input[data-type=email]');
-    const inputPassword = form.querySelector<HTMLInputElement>('input[data-type=password]');
-    const inputConfirmPassword = form.querySelector<HTMLInputElement>('input[data-type=confirmPassword]');
-    const inputFirstName = form.querySelector<HTMLInputElement>('input[data-type=firstName]');
-    const inputLastName = form.querySelector<HTMLInputElement>('input[data-type=lastName]');
-    form.addEventListener('submit', async event => {
-      event.preventDefault();
-      if (
-        inputEmail == null ||
-        inputFirstName == null ||
-        inputConfirmPassword == null ||
-        inputPassword == null ||
-        inputLastName == null
-      ) {
-        return;
-      }
-      const user = new User({
-        email: inputEmail.value,
-        password: inputPassword.value,
-        firstName: inputFirstName.value,
-        lastName: inputLastName.value,
-      });
-      const isValidPassword = validateConfirmPassword({ passwordElement: inputPassword, confirmPasswordElement: inputConfirmPassword },
-        'This field must be matched with password field');
-      if (!isValidPassword) {
-        return;
-      }
-      const result = await AuthorizationService.signUp(user);
-      if (result instanceof ErrorUser) {
-        queryErrorSpan(inputEmail, result.email);
-        queryErrorSpan(inputFirstName, result.firstName);
-        queryErrorSpan(inputLastName, result.lastName);
-        queryErrorSpan(inputPassword, result.password);
-      }
-    });
+  if (form == null) {
+    return;
   }
-
+  const inputEmail = form.querySelector<HTMLInputElement>('input[data-type=email]');
+  const inputPassword = form.querySelector<HTMLInputElement>('input[data-type=password]');
+  const inputConfirmPassword = form.querySelector<HTMLInputElement>('input[data-type=confirmPassword]');
+  const inputFirstName = form.querySelector<HTMLInputElement>('input[data-type=firstName]');
+  const inputLastName = form.querySelector<HTMLInputElement>('input[data-type=lastName]');
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+    if (
+      inputEmail == null ||
+      inputFirstName == null ||
+      inputConfirmPassword == null ||
+      inputPassword == null ||
+      inputLastName == null
+    ) {
+      return;
+    }
+    const user = new User({
+      email: inputEmail.value,
+      password: inputPassword.value,
+      firstName: inputFirstName.value,
+      lastName: inputLastName.value,
+    });
+    const isValidPassword = validateConfirmPassword({ passwordElement: inputPassword, confirmPasswordElement: inputConfirmPassword },
+      'This field must be matched with password field');
+    if (!isValidPassword) {
+      return;
+    }
+    const result = await AuthorizationService.signUp(user);
+    if (result instanceof ErrorUser) {
+      queryErrorSpan(inputEmail, result.email);
+      queryErrorSpan(inputFirstName, result.firstName);
+      queryErrorSpan(inputLastName, result.lastName);
+      queryErrorSpan(inputPassword, result.password);
+    }
+  });
 }
 
 validateRegisterInfo();
