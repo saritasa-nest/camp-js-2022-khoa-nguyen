ef7e87b919917dc36b4dbb7bbd7c4c8b3dc37b93
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-29 23:05:36 +0700

    Rename CSS class with BEM #JC19-358

--- apps/vanilla/src/pages/home/index.ts
+++ apps/vanilla/src/pages/home/index.ts
@@ -4,11 +4,9 @@ import { renderHeader } from '../../scripts/renderHeader';
 import { renderListOnActivePage } from '../../scripts/renderPagination';
 import { renderSearchingAndHandle } from '../../scripts/renderSearching';
 import { renderSortingAndOrdering } from '../../scripts/renderSortingAndOrdering';
-import { LocalStorageService } from '../../services/localStore';
 
 /** Render anime table. */
 function renderAnimeTable(): void {
-  LocalStorageService.clear();
   renderHeader();
   renderListOnActivePage(getInitialQueryParams());
   renderSortingAndOrdering();
