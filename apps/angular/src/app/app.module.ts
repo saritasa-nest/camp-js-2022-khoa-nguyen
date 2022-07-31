b419249011e9e321fa1d07fb2256ac51f08f4b8b
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-07-31 12:31:20 +0700

    Remove redundant import #JC19-413

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -4,12 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './../shared/shared.module';
