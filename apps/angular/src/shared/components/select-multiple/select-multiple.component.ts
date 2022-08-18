70b9abb0b68fa4d4e03018f2a241e761744c5163
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-08-18 17:44:59 +0700

    Add multiple select component #JC191-413

---
+++ apps/angular/src/shared/components/select-multiple/select-multiple.component.ts
@@ -0,0 +1,39 @@
+import { EventEmitter } from 'stream';
+
+import { ChangeDetectionStrategy, Component, Input, Output } from '@angular/core';
+
+interface OptionsList {
+
+  /** Value of options. */
+  value: string;
+
+  /** Title of options. */
+  title: string;
+}
+
+/** Select multiple component. */
+@Component({
+  selector: 'camp-select-multiple',
+  templateUrl: './select-multiple.component.html',
+  styleUrls: ['./select-multiple.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectMultipleComponent {
+
+  /** Value of select. */
+  @Input()
+  public value: string[] = [];
+
+  /** Value of select. */
+  @Input()
+  public optionsList: OptionsList[] = [];
+
+  /** On selection change event. */
+  @Output()
+  public selectionChange = new EventEmitter();
+
+  /** Emit selection event. */
+  public onSelectionChange(): void {
+    this.selectionChange.emit();
+  }
+}
--- apps/angular/src/shared/shared.module.ts
+++ apps/angular/src/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+import { SelectMultipleComponent } from './components/select-multiple/select-multiple.component';
 
 /** Shared module. */
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [SelectMultipleComponent],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule],
+  exports: [SelectMultipleComponent],
 })
 export class SharedModule {}
