1066555640901f6deb430fa1fb948af97e043478
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2022-09-05 11:34:21 +0700

    Edit docs #JC19-483

--- apps/react/src/hooks/useEffectSkipRender.ts
+++ apps/react/src/hooks/useEffectSkipRender.ts
@@ -1,7 +1,10 @@
 import { DependencyList, useEffect, useRef } from 'react';
 
-/** This hook is used for skip first rendering of the useEffect hook.
- *  Sometime, due to other factors, we need to skip two or more renders. */
+/**
+ * This hook is used for skip the amount of renders of the useEffect hook.
+ * Usually, we only need to skip 1 render but sometime, due to other factors,
+ * we might need to skip two or more renders. .
+ */
 
 export const useEffectSkipRender = (
   renderToBeSkipped: number,
